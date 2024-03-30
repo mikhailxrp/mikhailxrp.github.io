94233aba59740b162c48ce1a2592622e451b75cd
Mikhail <dmihail528@gmail.com>
2024-03-31 00:36:10 +0300

    refactor total show films

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -9,7 +9,7 @@ const Main = () => {
     const [isLoading, setIsLoading] = useState(true)
 
     async function getFilns() {
-        const response = await fetch(`https://api.kinopoisk.dev/v1.4/movie?page=1&limit=10&selectFields=id&selectFields=name&selectFields=enName&selectFields=shortDescription&selectFields=year&selectFields=rating&selectFields=genres&selectFields=countries&selectFields=poster&selectFields=videos&selectFields=lists&rating.kp=8-10`, {
+        const response = await fetch(`https://api.kinopoisk.dev/v1.4/movie?page=1&limit=20&selectFields=id&selectFields=name&selectFields=enName&selectFields=shortDescription&selectFields=year&selectFields=rating&selectFields=genres&selectFields=countries&selectFields=poster&selectFields=videos&selectFields=lists&rating.kp=8-10`, {
             method: 'GET',
             headers: {
                 "Content-Type": "application/json",
@@ -30,7 +30,7 @@ const Main = () => {
 
     async function getSearchFilm(film) {
         setIsLoading(true)
-        const response = await fetch(`https://api.kinopoisk.dev/v1.4/movie/search?page=1&limit=10&query=${film}`, {
+        const response = await fetch(`https://api.kinopoisk.dev/v1.4/movie/search?page=1&limit=20&query=${film}`, {
             method: 'GET',
             headers: {
                 "Content-Type": "application/json",
@@ -47,7 +47,7 @@ const Main = () => {
     }
 
     async function getTypeMovie(type) {
-        const response = await fetch(`https://api.kinopoisk.dev/v1.4/movie?page=1&limit=10&type=${type}&selectFields=id&selectFields=name&selectFields=enName&selectFields=shortDescription&selectFields=year&selectFields=rating&selectFields=genres&selectFields=countries&selectFields=poster&selectFields=videos&rating.kp=8-10`, {
+        const response = await fetch(`https://api.kinopoisk.dev/v1.4/movie?page=1&limit=20&type=${type}&selectFields=id&selectFields=name&selectFields=enName&selectFields=shortDescription&selectFields=year&selectFields=rating&selectFields=genres&selectFields=countries&selectFields=poster&selectFields=videos&rating.kp=8-10`, {
             method: 'GET',
             headers: {
                 "Content-Type": "application/json",
